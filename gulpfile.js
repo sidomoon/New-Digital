7412378e0eb77024f7df11f62b7b03faa17503a1
sidomoon <43311401+sidomoon@users.noreply.github.com>
2019-02-24 14:08:49 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+    var gulp       = require('gulp'),
+        less         = require('gulp-less'),
+        browserSync  = require('browser-sync'),
+        concat       = require('gulp-concat'),
+        cssnano      = require('gulp-cssnano'),
+        rename       = require('gulp-rename'),
+        del          = require('del'),
+        imagemin     = require('gulp-imagemin'),
+        cache        = require('gulp-cache'),
+        autoprefixer = require('gulp-autoprefixer');
+
+    gulp.task('less', function(){
+        return gulp.src('app/less/**/*.less')
+            .pipe(less())
+            .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload({stream: true}))
+    });
+
+    gulp.task('browser-sync', function() {
+        browserSync({
+            server: {
+                baseDir: 'app'
+            },
+            notify: false
+        });
+    });
+
+    gulp.task('css-libs', ['less'], function() {
+        return gulp.src('app/css/libs.css')
+            .pipe(cssnano())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('app/css'));
+    });
+
+    gulp.task('watch', ['browser-sync', 'css-libs'], function() {
+        gulp.watch('app/less/**/*.less', ['less']);
+        gulp.watch('app/*.html', browserSync.reload);
+        gulp.watch('app/js/**/*.js', browserSync.reload);
+    });
+
+    gulp.task('clean', function() {
+        return del.sync('dist');
+    });
+
+    gulp.task('img', function() {
+        return gulp.src('app/img/**/*')
+            .pipe(cache(imagemin({
+                interlaced: true,
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}]
+            })))
+            .pipe(gulp.dest('dist/img'));
+    });
+
+    gulp.task('build', ['clean', 'img', 'less'], function() {
+
+        var buildCss = gulp.src([
+            'app/css/main.css',
+            'app/css/libs.min.css'
+            ])
+        .pipe(gulp.dest('dist/css'))
+
+        var buildFonts = gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+
+        var buildJs = gulp.src('app/js/**/*')
+        .pipe(gulp.dest('dist/js'))
+
+        var buildHtml = gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'));
+
+    });
+
+    gulp.task('clear', function () {
+        return cache.clearAll();
+    })
+
+    gulp.task('default', ['watch']);
